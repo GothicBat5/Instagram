c6d1e8b0b4ded42569fb54c359694dca8537b00f
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-06-24 21:46:53 +0800

    Create docs.sh

---
+++ start/docs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if ! which jazzy >/dev/null; then
+  echo "Jazzy not detected: You can download it from https://github.com/realm/jazzy"
+  exit
+fi
+
+SOURCE=Source
+SOURCE_TMP=IGListKit
+SOURCEDIR=Source/
+
+jazzy \
+	--objc \
+	--clean \
+	--author 'Instagram' \
+    --author_url 'https://twitter.com/MetaOpenSource' \
+    --github_url 'https://github.com/Instagram/IGListKit' \
+    --sdk iphonesimulator \
+    --module 'IGListKit' \
+    --framework-root $SOURCEDIR/ \
+    --umbrella-header $SOURCEDIR/$SOURCE_TMP/IGListKit.h \
+    --readme README.md \
+    --documentation "Guides/*.md" \
+    --output docs/
