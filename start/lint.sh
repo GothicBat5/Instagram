093bf40632c6914a6da458c4bbc765c0cece8633
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-06-26 14:53:49 +0800

    Create lint.sh

---
+++ start/lint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if test -d "/opt/homebrew/bin/"; then
+  PATH="/opt/homebrew/bin/:${PATH}"
+  export PATH
+fi
+cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+
+VERSION="0.50.3"
+FOUND=$(swiftlint version)
+
+if which swiftlint >/dev/null; then
+    swiftlint lint --config ../.swiftlint.yml
+else
+    echo "
+    Warning: SwiftLint not installed!
+    You should download SwiftLint to verify your Swift code.
+    Download from https://github.com/realm/SwiftLint,
+    or brew install swiftlint.
+    "
+    exit
+fi
+if [ $(swiftlint version) != $VERSION ]; then
+    echo "
+    Warning: incorrect SwiftLint installed!
+    Expected: $VERSION
+    Found: $FOUND
+    Download from https://github.com/realm/SwiftLint,
+    or brew upgrade swiftlint.
+    "
+fi
+exit
